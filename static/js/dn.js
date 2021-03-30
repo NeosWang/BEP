272bc8ace78812419ffa95d74940910833524eb7
NeosWang <neos_wang@yahoo.com>
2021-03-31 01:49:49 +0200

    add Graph class and prototype functions in DynamicNetwork class

--- static/js/dn.js
+++ static/js/dn.js
@@ -1,36 +1,67 @@
 "use strict";
 function DynamicNetwork(serialized) {
 
+    function Graph(directed) {
+        this.edges = {};
+        this.directed = directed;
+    }
+    Graph.prototype = {
+        _addVertex: function (node) {
+            this.edges[node] = this._adjacent(node)
+        },
+        _adjacent: function (node) {
+            return this.edges[node] || []
+        },
+        _addEdge: function (u, v) {
+            this._addVertex(u);
+            this._addVertex(v);
+            this._adjacent(u).push(v)
+            if (!this.directed) { this._adjacent(v).push(u) }
+        },
+        _nrOfVertices: function () { return Object.keys(this.edges).length }
+    }
+
+
+
     if (serialized) {
         alert('shit');
     }
 
     var nodes = {}
-    var edges = {}
+    var relationships = {}
 
-    function _addNode(node) {
+    let _addNode = (node) => {
         nodes[node.id] = Object.keys(node).reduce((object, key) => {
-            if (key != 'id') {object[key] = node[key]}
+            if (key != 'id') { object[key] = node[key] }
             return object
-        }, {})
+        }, {});
     }
 
-    function addNodes(nodes) {
-        nodes.forEach(e => { _addNode(e) });
+    let addNodes = (nodes) => nodes.forEach(e => _addNode(e));
+
+    let _addRelationship = (edge, directed) => {
+        if (!relationships[edge.t]) { relationships[edge.t] = new Graph(directed); }
+        relationships[edge.t]._addEdge(edge.i, edge.j);
     }
 
-    // function _addEdge(edge){
-    //     edge
-    // }
+    let addRelationships = (edges, directed = false) => edges.forEach(e => _addRelationship(e, directed));
 
-    // function addEdges(){}
+    let getTimeline = () => { return Object.keys(relationships) }
 
+    let getListNrOfVertices = () => {
+        let output = []
+        Object.entries(relationships).forEach(([k, v]) => output.push(v._nrOfVertices()));
+        return output
+    }
 
 
     return {
         nodes: nodes,
-        edges: edges,
-        addNodes: addNodes
+        relationships: relationships,
+        addNodes: addNodes,
+        addRelationships: addRelationships,
+        getTimeline: getTimeline,
+        getListNrOfVertices:getListNrOfVertices,
     };
 }
 
